47c11701613f3647666d4a8f219dc3b523751df3
Tristan Hibbard <tristanhibbard@Tristans-MacBook-Air-2.local>
2024-04-10 13:41:01 -0400

    initial  commit

---
+++ Gitdemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tristan Hibbard
+ * 
+ */
+public class Gitdemo {
+
+	/**
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Dr. Johnson! This is <Tristan Hibbard>.");
+
+	}
+
+}
